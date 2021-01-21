5be7152ac95506a32cf6a900a953cfb50ce3b4e2
Anastasia Shpit <imshpit@gmail.com>
2021-01-21 23:17:12 +0200

    Mixed requests issue fixing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autoCompleteConfig = {
         return movie.Title;
     },
     async fetchData (searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('http://www.omdbapi.com', {
             params: {
                 apikey: '60f0684',
                 s: searchTerm
@@ -47,7 +47,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('http://www.omdbapi.com', {
         params: {
             apikey: '60f0684',
             i: movie.imdbID
